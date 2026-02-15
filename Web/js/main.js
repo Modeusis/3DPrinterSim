067e02f744dc345d59494bc2ac4998cbe21cfecd
Modeus <blastachanel@gmail.com>
2026-02-15 17:37:23 +0300

    init: first commit, lab #1 done

---
+++ Web/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('nav');
+
+    if (burger) {
+        burger.addEventListener('click', () => {
+            nav.classList.toggle('active');
+        });
+    }
+});
