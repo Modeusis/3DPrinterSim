ed87a13ee905839956849072f9c9d14740e26170
Modeusis <120785077+Modeusis@users.noreply.github.com>
2026-02-25 23:14:35 +0100

    feat: new lab2 done

--- Web/js/main.js
+++ Web/js/main.js
@@ -7,4 +7,39 @@ document.addEventListener('DOMContentLoaded', () => {
             nav.classList.toggle('active');
         });
     }
+
+    const enlargeableImgs = document.querySelectorAll('.enlargeable');
+    enlargeableImgs.forEach(img => {
+        img.addEventListener('click', function() {
+            this.classList.toggle('enlarged');
+        });
+        
+        img.addEventListener('mouseleave', function() {
+            this.classList.remove('enlarged');
+        });
+    });
+
+    const textReplacers = document.querySelectorAll('.text-replacer');
+    textReplacers.forEach(replacer => {
+        replacer.addEventListener('click', function() {
+            this.classList.add('active');
+        });
+        
+        replacer.addEventListener('mouseleave', function() {
+            this.classList.remove('active');
+        });
+    });
+
+    const partLinks = document.querySelectorAll('.part-link');
+    const targetPartImage = document.getElementById('target-part-image');
+    
+    if (partLinks.length > 0 && targetPartImage) {
+        partLinks.forEach(link => {
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
+                const newImageSrc = this.getAttribute('data-img');
+                targetPartImage.src = newImageSrc;
+            });
+        });
+    }
 });
